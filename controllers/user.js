f438419d5af57665b7c3ad73770e94bb935e5b4a
Faruq Khan <150416913+FaruqKhan11148@users.noreply.github.com>
2026-01-06 22:46:11 +0530

    updation

--- controllers/user.js
+++ controllers/user.js
@@ -26,7 +26,6 @@ module.exports.signup = async (req, res) => {
 
 // route for login form rendering
 module.exports.renderLoginForm = (req, res) => {
-  res.render('users/login');
 };
 
 // route for login form
