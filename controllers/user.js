7b950142a73f23d1ae358ed7552ebef2e92510ad
Faruq Khan <150416913+FaruqKhan11148@users.noreply.github.com>
2026-01-10 23:38:34 +0530

    updation

--- controllers/user.js
+++ controllers/user.js
@@ -29,7 +29,7 @@ module.exports.renderLoginForm = (req, res) => {
 
 // route for login form
 module.exports.login = async (req, res) => {
-  
+  req.flash('success', 'Welcom back to Fetify!');
   let redirectUrl = res.locals.redirectUrl || '/main';
   res.redirect(redirectUrl);
 };
